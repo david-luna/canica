ebc72157bbaa8c5c319988d9933ba29d706ee743
David Luna <luna.bistuer@gmail.com>
2021-07-03 01:24:21 +0200

    chore: preparations for the UI

---
+++ canica-electron/assets/preload.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld(
+  'backend',
+  {
+    dispatch: (command) => ipcRenderer.send(`annotatron:commands:${command.type}`, [command]),
+    events$: {
+      subscribe: (observer) => {
+        const ipcHandler = (evt, payload) => observer(payload);
+
+        ipcRenderer.on(`annotatron:results`, ipcHandler);
+        ipcRenderer.on(`annotatron:errors`, ipcHandler);
+        ipcRenderer.on(`annotatron:events`, ipcHandler);
+
+        return {
+          unsubscribe: function() {
+            ipcRenderer.removeListener(`annotatron:results`, ipcHandler);
+            ipcRenderer.removeListener(`annotatron:errors`, ipcHandler);
+            ipcRenderer.removeListener(`annotatron:events`, ipcHandler);
+          }
+        };
+      }
+    }
+  }
+);
